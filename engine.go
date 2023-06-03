af2c7b992805d100d962f2811cfb1aea3f79ca3d
Brian Sterle <sterle.brian@gmail.com>
2023-06-03 18:30:42 -0400

    Projection/Selection/Aggreagate

--- engine.go
+++ engine.go
@@ -289,3 +289,71 @@ func (s *Scan) String() string {
 	}
 	return fmt.Sprintf("Scan: %s; projection=%v", s.Path, s.Projection)
 }
+
+type Projection struct {
+	Input LogicalPlan
+	Expr  []LogicalExpr
+}
+
+func (p *Projection) Schema() Schema {
+	fields := []arrow.Field{}
+	for _, e := range p.Expr {
+		fields = append(fields, e.toField(p.Input))
+	}
+	return Schema{arrow.NewSchema(fields, nil)}
+}
+
+func (p *Projection) Children() []LogicalPlan {
+	return []LogicalPlan{p.Input}
+}
+
+func (p *Projection) String() string {
+	strs := make([]string, len(p.Expr))
+	for _, e := range p.Expr {
+		strs = append(strs, e.String())
+	}
+	s := strings.Join(strs, ", ")
+	return fmt.Sprintf("Projection: %s", s)
+}
+
+type Selection struct {
+	Input LogicalPlan
+	Expr  LogicalExpr
+}
+
+func (s *Selection) Schema() Schema {
+	return s.Input.Schema()
+}
+
+func (s *Selection) Children() []LogicalPlan {
+	return []LogicalPlan{s.Input}
+}
+
+func (s *Selection) String() string {
+	return fmt.Sprintf("Filter: %s", s.Expr.String())
+}
+
+type Aggregate struct {
+	Input         LogicalPlan
+	GroupExpr     []LogicalExpr
+	AggregateExpr []AggregateExpr
+}
+
+func (a *Aggregate) Schema() Schema {
+	fields := []arrow.Field{}
+	for _, e := range a.GroupExpr {
+		fields = append(fields, e.toField(a.Input))
+	}
+	for _, e := range a.AggregateExpr {
+		fields = append(fields, e.toField(a.Input))
+	}
+	return Schema{arrow.NewSchema(fields, nil)}
+}
+
+func (a *Aggregate) Children() []LogicalPlan {
+	return []LogicalPlan{a.Input}
+}
+
+func (a *Aggregate) String() string {
+	return fmt.Sprintf("Aggregate: groupExpr=%s, aggregateExpr=%s", a.GroupExpr, a.AggregateExpr)
+}
