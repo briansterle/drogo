f98296fce766f59aa7aaad32b8e557da6fa32e4a
Brian Sterle <sterle.brian@gmail.com>
2023-06-04 18:44:08 -0400

    arrowtype

---
+++ arrowtype/arrowtype.go
@@ -0,0 +1,16 @@
+package arrowtype
+
+import (
+	"github.com/apache/arrow/go/arrow"
+)
+
+var (
+	Int8    = &arrow.Int8Type{}
+	Int16   = &arrow.Int16Type{}
+	Int32   = &arrow.Int32Type{}
+	Int64   = &arrow.Int64Type{}
+	Float32 = &arrow.Float32Type{}
+	Float64 = &arrow.Float64Type{}
+	String  = &arrow.StringType{}
+	Boolean = &arrow.BooleanType{}
+)
